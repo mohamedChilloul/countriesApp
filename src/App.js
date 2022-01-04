d1abd8d1f549273ae3ab4a7e6a7251351cd718c7
mohamedChilloul <m.chilloul@esi-sba.dz>
2022-01-04 22:57:46 +0100

    step 2 of the countries app , show details for a country

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, {useState, useEffect} from "react";
 import axios from 'axios'
-import DisplayCountries from "./components/DisplayCountries";
+import DisplayCountry from "./components/DisplayCountry";
 
 function App() {
   const [countries ,  setCountries] = useState([])
   const [filter, setFilter] = useState('')
+  const [country, setCountry] = useState(0)
   const fetchCountries = ()=>{
     axios.get('http://localhost:3001/countries').then(r=>{
       console.log(r.data.length,' countries goted !')
@@ -18,13 +19,41 @@ function App() {
   const filtredList = countries.filter(c =>{
     return c.name.common.toLocaleLowerCase().match(filter.toLocaleLowerCase())
   })
-  console.log(filtredList.length, ' matched !')
-  return (
+  const listLength = filtredList.length
+
+  const handleShow = (e)=>{
+    setCountry(e.target.value)
+  }
+    return (
     <div>
       <div>
         search for : <input value={filter} onChange={handleChangeFilter}></input>
       </div>
-      <DisplayCountries filtredCountries={filtredList}></DisplayCountries>
+      <div>
+        {
+          listLength ===1 ?
+          <DisplayCountry country={filtredList[0]}></DisplayCountry> 
+                          :
+          listLength <=10 ? 
+            filtredList.map(
+              (c,i) =><li  key={i}>
+                  {c.name.common} <button value={i} onClick={handleShow}>show</button>
+                      </li>
+              )
+                          :
+          <p>more then 10 countries matched for -- {filter} -- keyWord !</p>
+          
+        }
+      </div>
+      <div>
+        {
+          listLength ===0 ?
+          <p>No country to display</p>
+                          :
+          <DisplayCountry country={filtredList[country]}></DisplayCountry>
+
+        }
+      </div>
     </div>
   );
 }
